af8d4c48f4163c6c10a249e6f670641d88c7806c
wiltonvgc <wiltondesc@gmail.com>
2016-12-08 21:17:00 -0200

    criacao do cabeçalho

--- cgi-bin/daemon.py
+++ cgi-bin/daemon.py
@@ -1,6 +1,26 @@
 from socket import *
 from thread import *
 
+#Funcao que reecebe mensagem de bits enviados e retorna dict com valores inteiros para cada campos do cabecalho
+def decodificaMensagem(msg):
+	dic = {}
+	dic['version'] = msg[0:4]
+	dic['ihl'] = msg[4:8]
+	dic['type_of_service'] = msg[8:16]
+	dic['total_length'] = msg[16:32]
+	dic['identification'] = msg[32:48]
+	dic['flags'] = msg[48:51]
+	dic['fragment_offset'] = msg[51:64]
+	dic['time_to_live'] = msg[64:72]
+	dic['protocol'] = msg[73:81]
+	dic['header_checksum'] = msg[81:97]
+	dic['source_address'] = msg[97:129]
+	dic['destination_address'] = msg[129:161]
+	tamanho = len(msg)
+	dic['options'] = msg[161:tamanho-8]
+	dic['padding'] = msg[tamanho-8:tamanho]
+	return dic
+
 #Inicia daemon TCP com socket
 def startServerTCP(host, port):
 
@@ -14,7 +34,8 @@ def startServerTCP(host, port):
 	while True:
 		conexao, cliente = tcp.accept()
 		mensagem = conexao.recv(1024)
-		conexao.send(mensagem)
+		d = decodificaMensagem(mensagem)
+		conexao.send("PROTOCOL : " + d['version'])
 		conexao.close()
 			
 
--- cgi-bin/webserver.py
+++ cgi-bin/webserver.py
@@ -4,6 +4,7 @@ import cgitb
 import cgi
 from ctypes import *
 from socket import *
+from thread import *
 
 
 #Define funcao inicia conexao Socket Cliente TCP
@@ -13,8 +14,6 @@ def startClientSocket(host,port,msg):
 	destino = (host,port)
 
 	tcp.connect(destino)
-	
-	
 
 	tcp.send(msg)
 
@@ -25,6 +24,33 @@ def startClientSocket(host,port,msg):
 	return msg_recebida
 
 
+#Define funcao que cria pacote para envio 
+#Entrada: recebe campos do cabecalho
+#Retorno: string de bits relativos aos campos respeitando o cabeçalho padrao
+def criaPacote(version, ihl, type_of_service, total_length, identification, flags, fragment_offset, time_to_live, protocol, header_checksum, source_address, destination_address, options, padding):
+	vers = "{:04b}".format(version)
+	ih =  "{:04b}".format(ihl)
+	ts =  "{:08b}".format(type_of_service)
+	tl = "{:016b}".format(total_length)
+	ide =  "{:016b}".format(identification)
+	fl =  "{:03b}".format(flags)
+	frag =  "{:013b}".format(fragment_offset)
+	time =  "{:08b}".format(time_to_live)
+	prot =  "{:08b}".format(protocol)
+	check =  "{:016b}".format(header_checksum)
+	source =  "{:032b}".format(source_address)
+	dest = "{:032b}".format(destination_address)
+	padd =  "{:08b}".format(padding)
+	
+	s = ""
+	for c in options:
+		s =  s + "{:08b}".format(ord(c))
+	
+	pacote = vers + ih + ts + tl + ide + fl + frag + time + prot + check + source + dest + s + padd
+	return pacote
+
+
+"""
 #Define estrutura para cabeçalho do pacote de dados
 class Header(Structure):
 	_fields_ = [("version",c_uint,4), ("ihl",c_uint,4), ("type-of-service",c_uint,8), ("total-lenght",c_uint,16), ("identification",c_uint,16), ("flags",c_uint, 3), ("fragment-offset",c_uint,13), ("time-to-live",c_uint,16), ("protocol",c_uint,8),
@@ -79,7 +105,7 @@ def buildDictFields(version, ihl, type_of_service, total_lenght, identification,
 	values['padding'] = padding
 	return values
 	
-
+"""
 def main():
 	#hablita cgi
 	cgitb.enable()    
@@ -90,30 +116,153 @@ def main():
 
 	form = cgi.FieldStorage()
 
-	#Maquina 1 
-	#maq1_ps = form.getvalue("maq1_ps")
-	#maq1_ps_t = form.getvalue("maq1-ps")
-
-	#print(maq1_ps_t)
 	
-	#maq1_df = form["maq1_df"].value
-	#maq1_df_t = form["maq1-df"].value
 
-	#maq1_finger = form["maq1_finger"].value
-	#maq1_finger_t = form["maq1-finger"].value
 
-	#maq1_uptime = form["maq1_uptime"].value
-	#maq1_uptime_t = form["maq1-uptime"].value
-
-	msg = startClientSocket('192.168.1.102',9001,"msg1")
-	print(msg)
-	msg = startClientSocket('192.168.1.102',9001,"msg2")
-	print(msg)
+	# Contrucao de pacote para MAQUINA 1 
 	
-	msg = startClientSocket('192.168.1.102',9002,"msg3")
-	print(msg)
-	msg = startClientSocket('192.168.1.102',9002,"msg4")
-	print(msg)
+	#COMANDO PS - MAQUINA 1	
+	
+	maq1_ps = form.getvalue("maq1_ps")
+	maq1_ps_t = form.getvalue("maq1-ps")
+	if(maq1_ps=='ps'):
+		protocol = 1
+		version = 2
+		ihl = 24
+		type_of_service = 0
+		identification = 1
+		flags = 0
+		fragment_offset = 0
+		time_to_live = 1
+		source_address = 3232235881
+		destination_address = 3232235879
+		header_checksum = 0
+		options = ""
+		padding = 0
+		# se parametros estao vazios
+		if not maq1_ps_t:
+			total_length = 24 #bytes
+			msg = criaPacote(version, ihl, type_of_service, total_length, identification,flags, fragment_offset,time_to_live, protocol, header_checksum, source_address, destination_address, options, padding)
+			html = startClientSocket('192.168.1.103',9001,msg)
+			print(html)
+			
+		else:
+			options = maq1_ps_t
+			total_length = 21
+			for caracter in maq1_ps_t:
+				total_length = total_length + 1
+			msg = criaPacote(version, ihl, type_of_service, total_length, identification,flags, fragment_offset,time_to_live, protocol, header_checksum, source_address, destination_address, options, padding)
+			html = startClientSocket('192.168.1.103',9001,msg)
+			print(html)
+			
+
+
+	#COMANDO DF - MAQUINA 1	
+	
+	maq1_df = form.getvalue("maq1_df")
+	maq1_df_t = form.getvalue("maq1-df")
+	if(maq1_df=='df'):
+		print("DF")
+		protocol = 2
+		version = 2
+		ihl = 24
+		type_of_service = 0
+		identification = 1
+		flags = 0
+		fragment_offset = 0
+		time_to_live = 1
+		source_address = 3232235881
+		destination_address = 3232235879
+		header_checksum = 0
+		options = ""
+		padding = 0
+		# se parametros estao vazios
+		if not maq1_df_t:
+			total_length = 24 #bytes
+			msg = criaPacote(version, ihl, type_of_service, total_length, identification,flags, fragment_offset,time_to_live, protocol, header_checksum, source_address, destination_address, options, padding)
+			html = startClientSocket('192.168.1.103',9001,msg)
+			print(html)
+		else:
+			options = maq1_df_t
+			total_length = 21
+			for caracter in maq1_df_t:
+				total_length = total_length + 1
+			msg = criaPacote(version, ihl, type_of_service, total_length, identification,flags, fragment_offset,time_to_live, protocol, header_checksum, source_address, destination_address, options, padding)
+			html = startClientSocket('192.168.1.103',9001,msg)
+			print(html)
+	
+
+
+	#COMANDO FINGER - MAQUINA 1	
+	
+	maq1_finger = form.getvalue("maq1_finger")
+	maq1_finger_t = form.getvalue("maq1-finger")
+	if(maq1_finger=='finger'):
+		print("FINGER")
+		protocol = 3
+		version = 2
+		ihl = 24
+		type_of_service = 0
+		identification = 1
+		flags = 0
+		fragment_offset = 0
+		time_to_live = 1
+		source_address = 3232235881
+		destination_address = 3232235879
+		header_checksum = 0
+		options = ""
+		padding = 0
+		# se parametros estao vazios
+		if not maq1_finger_t:
+			total_length = 24 #bytes
+			msg = criaPacote(version, ihl, type_of_service, total_length, identification,flags, fragment_offset,time_to_live, protocol, header_checksum, source_address, destination_address, options, padding)
+			html = startClientSocket('192.168.1.103',9001,msg)
+			print(html)
+		else:
+			options = maq1_finger_t
+			total_length = 21
+			for caracter in maq1_finger_t:
+				total_length = total_length + 1
+			msg = criaPacote(version, ihl, type_of_service, total_length, identification,flags, fragment_offset,time_to_live, protocol, header_checksum, source_address, destination_address, options, padding)
+			html = startClientSocket('192.168.1.103',9001,html)
+			print(html)
+
+
+	#COMANDO UPTIME - MAQUINA 1	
+	
+	maq1_uptime = form.getvalue("maq1_uptime")
+	maq1_uptime_t = form.getvalue("maq1-uptime")
+	if(maq1_uptime=='uptime'):
+		print("UPTIME")
+		protocol = 4
+		version = 2
+		ihl = 24
+		type_of_service = 0
+		identification = 1
+		flags = 0
+		fragment_offset = 0
+		time_to_live = 1
+		source_address = 3232235881
+		destination_address = 3232235879
+		header_checksum = 0
+		options = ""
+		padding = 0
+		# se parametros estao vazios
+		if not maq1_uptime_t:
+			total_length = 24 #bytes
+			msg = criaPacote(version, ihl, type_of_service, total_length, identification,flags, fragment_offset,time_to_live, protocol, header_checksum, source_address, destination_address, options, padding)
+			html = startClientSocket('192.168.1.103',9001,msg)
+			print(html)
+		else:
+			options = maq1_uptime_t
+			total_length = 21
+			for caracter in maq1_uptime_t:
+				total_length = total_length + 1
+			msg = criaPacote(version, ihl, type_of_service, total_length, identification,flags, fragment_offset,time_to_live, protocol, header_checksum, source_address, destination_address, options, padding)
+			html = startClientSocket('192.168.1.103',9001,msg)
+			print(enviaMensagem(html))
+
+
 
 
 	
